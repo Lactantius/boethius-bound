f5c67b142a43b7372c9976346f9e6d7bae47e51e
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-25 17:25:09 -0500

    Validate for font record in ProjectItem

--- test/models/project_item_test.rb
+++ test/models/project_item_test.rb
@@ -10,4 +10,9 @@ class ProjectItemTest < ActiveSupport::TestCase
     assert @project_item.valid?
   end
 
+  test "should reject missing font" do
+    @project_item.font = ''
+    refute @project_item.valid?
+  end
+
 end
