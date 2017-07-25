e31537faeb8285d983cdd0c858311974e7865a9e
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-25 17:32:55 -0500

    Add validations that book and project are present for ProjectItem

--- app/models/project_item.rb
+++ app/models/project_item.rb
@@ -3,5 +3,21 @@ class ProjectItem < ApplicationRecord
   belongs_to :book
 
   validates :font, presence: true
+  validate :project_present
+  validate :book_present
+
+  private
+
+  def project_present
+    if project.nil?
+      errors.add(:project, "is missing")
+    end
+  end
+
+  def book_present
+    if book.nil?
+      errors.add(:book, "is missing")
+    end
+  end
 
 end
