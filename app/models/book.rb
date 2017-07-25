7a802b66c1aee57fd3655ae798fce9854ad291d9
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-25 15:04:53 -0500

    Fix should destroy book test by adding dependent: :destroy

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :project_items
+  has_many :project_items, dependent: :destroy
   validates :pubdate, presence: true,
                       numericality: {only_integer: true }
   validates :title, :author, :location, :language, :converter, presence: true
