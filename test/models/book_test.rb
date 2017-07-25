0c350b840c452efcf9c06456f5e5f0ed66df481b
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-25 12:42:23 -0500

    Add validation test for Book model

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @book = Book.new(title: "Adventures of Bob", author: "Bob Bobson",
+                     location: "/dev/null", converter: "BOB",
+                     language: "english", pubdate: 1875)
+  end
+
+  test "should be valid" do
+    assert @book.valid?
+  end
+
 end
