255ae0ed9caf809dbdb611feaf0db4b748ddabe2
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-25 17:37:46 -0500

    Add current_project helper method to ApplicationControler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_project
+
+  def current_project
+    if !session[:project_id].nil?
+      Project.find(session[:project_id])
+    else
+      Project.new
+    end
+
 end
