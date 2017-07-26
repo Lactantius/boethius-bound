7d9e99538c26d795d9d98c72c4bc43d1161eab1b
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-26 11:23:43 -0500

    Add routes for ProjectItem and Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :books
+  resources :project_items
+  resources :projects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
   get 'home' => 'static_pages#home'
