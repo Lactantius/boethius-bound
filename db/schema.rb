21a93adb3071f196edcdf07430b7ce2b1b341b89
Gerard Keiser <gerardkeiser@gmail.com>
2017-07-27 18:04:15 -0500

    Add name column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727222025) do
+ActiveRecord::Schema.define(version: 20170727230315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170727222025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "name"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
