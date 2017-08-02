0fc24939eb0d041b2ade7cb9de189d61b851f3db
Gerard Keiser <gerardkeiser@gmail.com>
2017-08-02 13:48:00 -0500

    Fix broken project item controller

--- app/controllers/project_items_controller.rb
+++ app/controllers/project_items_controller.rb
@@ -2,9 +2,12 @@ class ProjectItemsController < ApplicationController
 
   def create
     @project = current_project
-    @project_item = @project.project_items.new(project_item_params)
-    @project.save
-    session[:project_id] = @project.id
+    @project_item = @project.project_items.build(project_item_params)
+    if @project_item.save
+      redirect_to @project, notice: 'Book added successfully.'
+    else
+      redirect_to @project, notice: 'Book was not added successfully.'
+    end
   end
 
   def update
