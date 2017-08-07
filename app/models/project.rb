b22e5d47ca80d60f102a88cdfa2192b888dbc72e
Gerard Keiser <gerardkeiser@gmail.com>
2017-08-07 14:45:37 -0500

    Add to_hash method to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,16 @@ class Project < ApplicationRecord
   belongs_to :user
   validates :user_id, :name, presence: true
   has_many :project_items, dependent: :destroy
+
+  # It seems like there should be a better way.
+  def to_hash
+    attributes = self.attributes
+    attributes[:project_items] = self.project_items.each_with_object([]) do |item, array|
+      item_attributes = item.attributes
+      item_attributes[:book] = item.book.attributes
+      array << item_attributes
+    end
+    attributes.deep_symbolize_keys
+  end
+
 end
