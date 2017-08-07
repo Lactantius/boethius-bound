67a9b55ca93511c46da5f85291d46c8fe746a4ca
Gerard Keiser <gerardkeiser@gmail.com>
2017-08-07 15:46:31 -0500

    Make clumsy test for Project.to_hash

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,10 +6,10 @@ class Project < ApplicationRecord
   # It seems like there should be a better way.
   def to_hash
     attributes = self.attributes
-    attributes[:project_items] = self.project_items.each_with_object([]) do |item, array|
+    attributes[:project_items] = self.project_items.map do |item|
       item_attributes = item.attributes
       item_attributes[:book] = item.book.attributes
-      array << item_attributes
+      item_attributes
     end
     attributes.deep_symbolize_keys
   end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "to_hash function" do
+    @project = projects(:one)
+    @item_one = @project.project_items.build(project_items(:one)
+                                      .attributes)
+    @item_two = @project.project_items.build(project_items(:two)
+                                      .attributes)
+    tex = @project.to_hash
+    assert tex[:project_items].first[:book][:title] == 'Bob’s Book'
+    assert_not tex[:project_items].second[:book][:title] == 'Bob’s Book'
+   end
+
 end
