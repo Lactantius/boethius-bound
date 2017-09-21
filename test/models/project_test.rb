0d19402727a2e185ed834d5e3abd20d1667e4478
Gerard Keiser <gerardkeiser@gmail.com>
2017-09-21 14:33:07 -0500

    Get to_hash function test working by fixing fixtures

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,7 +4,6 @@ class ProjectTest < ActiveSupport::TestCase
  
   # Having trouble with fixtures.
   test "to_hash function" do
-    skip
     @project = projects(:one)
     @item_one = @project.project_items.build(project_items(:one).attributes)
     @item_two = @project.project_items.build(project_items(:two).attributes)
