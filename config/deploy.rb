24e1cd89738a2e687a11563c31897112c29f124c
Gerard Keiser <gerardkeiser@gmail.com>
2018-02-11 13:35:08 -0600

    Tell Capistrano to ask for branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "boethius_bound"
 set :repo_url, "git@github.com:lactantius/boethius-bound.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/srv/#{fetch(:application)}"
