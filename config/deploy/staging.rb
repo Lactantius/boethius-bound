c8091ee25c6dc83874e31812d1983e8207b0ea23
Gerard Keiser <gerardkeiser@gmail.com>
2018-03-31 12:56:50 -0500

    Add staging environment

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '192.168.144.2', user: 'vagrant', roles: %w{app db web}
+server 'boethiusbound.dev', user: 'vagrant', roles: %w{app db web}
 
 # role-based syntax
 # ==================
